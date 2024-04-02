09024a955755f7106bcca7820adb68a1342e4bf3
tylerslaton <mtslaton1@gmail.com>
2024-04-02 19:15:48 -0400

    feat: comment out story deletion logic for hosting purposes
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- components/Stories.vue
+++ components/Stories.vue
@@ -9,33 +9,33 @@ const stories = computed(() => store.stories)
 onMounted(async () => { store.fetchStories() })
 const goToStory = (name: string) => useRouter().push(`/story/${name}`)
 
-const deleteStory = async (name: string) => {
-    const response = await fetch(`/api/story/${name}`, { method: 'DELETE' })
-    if (response.ok) {
-        store.removeStory(name)
-        toast.add({
-            id: 'story-deleted',
-            title: `${name} deleted`,
-            description: 'The story has been deleted.',
-            icon: 'i-heroicons-trash',
-        })
-    } else {
-        toast.add({
-            id: 'story-delete-failed',
-            title: `Failed to delete ${name}`,
-            description: 'The story could not be deleted.',
-            icon: 'i-heroicons-x-mark',
-        })
-    }
-}
+// const deleteStory = async (name: string) => {
+//     const response = await fetch(`/api/story/${name}`, { method: 'DELETE' })
+//     if (response.ok) {
+//         store.removeStory(name)
+//         toast.add({
+//             id: 'story-deleted',
+//             title: `${name} deleted`,
+//             description: 'The story has been deleted.',
+//             icon: 'i-heroicons-trash',
+//         })
+//     } else {
+//         toast.add({
+//             id: 'story-delete-failed',
+//             title: `Failed to delete ${name}`,
+//             description: 'The story could not be deleted.',
+//             icon: 'i-heroicons-x-mark',
+//         })
+//     }
+// }
 </script>
 
 <template>
     <div class="flex flex-col space-y-2">        
         <div class="w-full" v-for="story in stories" >
             <!-- The LLM likes to ocassionally, not always, generate folders with "-" and other times with " ", handle both cases for the button -->
-            <UButton truncate :key="story" size="lg" class="w-5/6 text-xl" :label="unmangleStoryName(story)" icon="i-heroicons-book-open" @click="goToStory(story)"/>
-            <UButton size="lg" variant="ghost" class="w-1/6 text-xl" icon="i-heroicons-trash" @click="deleteStory(story)"/>
+            <UButton truncate :key="story" size="lg" class="w-full text-xl" :label="unmangleStoryName(story)" icon="i-heroicons-book-open" @click="goToStory(story)"/>
+            <!-- <UButton size="lg" variant="ghost" class="w-1/6 text-xl" icon="i-heroicons-trash" @click="deleteStory(story)"/> -->
         </div>
     </div>
 </template>
--- server/api/story/[name].delete.ts
+++ server/api/story/[name].delete.ts
@@ -1,31 +1,31 @@
 import fs from 'fs'
 
 export default defineEventHandler(async (event) => {
-    try {
-        let name = getRouterParam(event, 'name')
-        if (!name) {
-            throw createError({
-                statusCode: 400,
-                statusMessage: 'name is required'
-            });
-        }
+    // try {
+    //     let name = getRouterParam(event, 'name')
+    //     if (!name) {
+    //         throw createError({
+    //             statusCode: 400,
+    //             statusMessage: 'name is required'
+    //         });
+    //     }
 
-        name = decodeURIComponent(name);
+    //     name = decodeURIComponent(name);
 
-        const path = useRuntimeConfig().storiesVolumePath
-        await fs.promises.readdir(`${path}/${name}`)
-        fs.promises.rm(`${path}/${name}`, { recursive: true })
-    } catch (error) {
-        // if the error is a 404 error, we can throw it directly
-        if ((error as any).code === 'ENOENT') {
-            throw createError({
-                statusCode:    404,
-                statusMessage: 'story not found',
-            })
-        }
-        throw createError({
-            statusCode:    500,
-            statusMessage: `error removing story: ${error}`,
-        })
-    }
+    //     const path = useRuntimeConfig().storiesVolumePath
+    //     await fs.promises.readdir(`${path}/${name}`)
+    //     fs.promises.rm(`${path}/${name}`, { recursive: true })
+    // } catch (error) {
+    //     // if the error is a 404 error, we can throw it directly
+    //     if ((error as any).code === 'ENOENT') {
+    //         throw createError({
+    //             statusCode:    404,
+    //             statusMessage: 'story not found',
+    //         })
+    //     }
+    //     throw createError({
+    //         statusCode:    500,
+    //         statusMessage: `error removing story: ${error}`,
+    //     })
+    // }
 })
