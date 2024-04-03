d719841aff20dc391d933f35dd47f68d187d1214
tylerslaton <mtslaton1@gmail.com>
2024-04-03 10:52:28 -0400

    feat: add links for source code and GPTScript to nav
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 const isMenuOpen = ref(false)
+const openSource = () => window.open('https://github.com/gptscript-ai/story-book', '_blank')
+const openGPTScript = () => window.open('https://github.com/gptscript-ai/gptscript', '_blank')
 </script>
 
 <template>
@@ -18,8 +20,13 @@ const isMenuOpen = ref(false)
                         <UButton size="lg" icon="i-heroicons-x-mark-20-solid" color="white" @click="isMenuOpen = false" />
                     </div>    
                 </div>
+                <UDivider class="mt-4"/>
+                <h3 class="text-2xl mt-4">Navigation</h3>
                 <UButton color="gray" class="mt-6 text-xl w-full" size="xl" label="Home" icon="i-heroicons-home" @click="() => { useRouter().push('/'); isMenuOpen = false }"/>
+                <UButton color="gray" class="mt-2 text-xl w-full" size="xl" label="Source Code" icon="i-heroicons-arrow-top-right-on-square" @click="() => { openSource(); isMenuOpen = false }"/>
+                <UButton color="gray" class="mt-2 text-xl w-full" size="xl" label="GPTScript" icon="i-heroicons-arrow-top-right-on-square" @click="() => { openGPTScript(); isMenuOpen = false }"/>
             </template>
+            <h3 class="text-2xl mb-6">Stories</h3>
             <Stories @click="(isMenuOpen = false)"/>
         </UCard>
     </USlideover>
